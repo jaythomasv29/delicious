eb2c0012e9a480f5b8e45f6d364d99141df2c83a
James Vongampai <jaythomasv29@gmail.com>
2022-04-22 16:30:08 -0700

    add recipe page to render data and navlink to direct user to page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -3,8 +3,20 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function Recipe() {
+    const params = useParams()
+    const [recipeData, setRecipeData] = useState({})
+    
+    useEffect(() => {
+        const getRecipeData = async (id) => {
+            const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}
+            `)
+            const data = await response.json()
+            console.log(data)
+        }
+        getRecipeData(params.name)
+    }, [])
   return (
-    <div>Recipe</div>
+    <div>{params.name}</div>
   )
 }
 
